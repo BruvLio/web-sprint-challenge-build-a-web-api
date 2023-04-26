fda31c42910ec8d97abb7ff8ec9991e5bb186980
BruvLio <lionel.mbayu@gmail.com>
2023-04-26 13:15:41 -0500

    Connected actions router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,10 @@
 // Write your "actions" router here!
+
+const express = require("express");
+
+const actions = require("./actions-model");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {});
+module.exports = router;
