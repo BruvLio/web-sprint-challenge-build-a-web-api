2d5b02c299bd6a61453d6e0539fd01f934f2478c
BruvLio <lionel.mbayu@gmail.com>
2023-04-26 12:19:14 -0500

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -14,8 +14,35 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  console.log("GET BY ID");
+router.get("/:id", async (req, res, next) => {
+  try {
+    const project = await Project.get(req.params.id);
+    if (!project) {
+      res.status(404).json({
+        message: `No project with provided ${req.params.id}`,
+      });
+    } else {
+      res.status(200).json(project);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, description } = req.body;
+    if (!name || !description) {
+      res.status(400).json({
+        message: `Bad request needs name and description`,
+      });
+    } else {
+      const newProject = await Project.insert(req.body);
+      res.status(201).json(newProject);
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
