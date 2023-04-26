061f20feab78ee941788fb53c8316d158e367993
BruvLio <lionel.mbayu@gmail.com>
2023-04-26 13:15:51 -0500

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,13 +1,13 @@
 // Write your "projects" router here!
 const express = require("express");
 
-const Project = require("./projects-model");
+const Projects = require("./projects-model");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const projects = await Project.get();
+    const projects = await Projects.get();
     res.status(200).json(projects);
   } catch (err) {
     next(err);
@@ -16,7 +16,7 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
   try {
-    const project = await Project.get(req.params.id);
+    const project = await Projects.get(req.params.id);
     if (!project) {
       res.status(404).json({
         message: `No project with provided ${req.params.id}`,
@@ -32,7 +32,7 @@ router.get("/:id", async (req, res, next) => {
 router.get("/:id/actions", async (req, res, next) => {
   try {
     const { id } = req.params;
-    const actions = await Project.getProjectActions(id);
+    const actions = await Projects.getProjectActions(id);
     res.status(200).json(actions);
   } catch (err) {
     next(err);
@@ -47,7 +47,7 @@ router.post("/", async (req, res, next) => {
         message: `Bad request needs name and description`,
       });
     } else {
-      const newProject = await Project.insert(req.body);
+      const newProject = await Projects.insert(req.body);
       res.status(201).json(newProject);
     }
   } catch (err) {
@@ -58,14 +58,14 @@ router.post("/", async (req, res, next) => {
 router.delete("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
-    const project = await Project.get(id);
+    const project = await Projects.get(id);
     if (!project) {
       res.status(404).json({
         message: `No project with provided ${id}`,
       });
     } else {
-      await Project.remove(id);
-      const projects = await Project.get();
+      await Projects.remove(id);
+      const projects = await Projects.get();
       res.status(200).json(projects);
     }
   } catch (err) {
